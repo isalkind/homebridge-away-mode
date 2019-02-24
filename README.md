c66b4bfbac979ba3843c069a39ed2e07c5ce363d
Ian Salkind <isalkind@mac.com>
2019-02-23 22:44:57 -0800

    Add ability to restrict when triggers fire
    If configured, triggers will only turn on between the startTime and
    endTime.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Homebridge plugin that provides triggers to turn on and turn off lights to simulate occupancy. We provide the triggers, you provide the lights.
 
-A simulated switch is created that controls whether "away mode" is active. When the switch is on, away mode is active. When the switch is off, away mode is inactive. A set of simulated sensors detect "activity". When activity (motion) is detected, turn the light on. When activity (motion) is not detected, turn the light off. The behavior of each sensor is random: a sensor is off for a period of time, turns on for a period of time, then repeats. When the switch is turned on, the sensors are activiated to start their off/on behavior. When the switch is turned off, the sensors are deactived and turned off.
+A simulated switch is created that controls whether "away mode" is active. When the switch is on, away mode is active. When the switch is off, away mode is inactive. A set of simulated sensors detect "activity". When activity (motion) is detected, turn the light on. When activity (motion) is not detected, turn the light off. The behavior of each sensor is random: a sensor is off for a period of time, turns on for a period of time, then repeats. When the switch is turned on, the sensors are activated to start their off/on behavior. When the switch is turned off, the sensors are deactivated and turned off.
 
 # Installation
 
@@ -10,14 +10,17 @@ npm -g install homebridge-away-mode
 
 # Configuration
 
-Example config.json:
-
+### Simple configuration example
+config.json:
 ```
 "accessories": [
     {
         "accessory": "AwayMode",
         "name": "Away Mode",
-        "sensorNames": ["Trigger 1", "Trigger 2"],
+        "sensorNames": [
+            "Trigger 1",
+            "Trigger 2"
+        ],
         "minOffTime": 300,
         "maxOffTime": 1800,
         "minOnTime": 1800,
@@ -25,13 +28,96 @@ Example config.json:
     }
 ]
 ```
+The above example creates a switch called 'Away Mode' and two sensors: 'Trigger 1' and 'Trigger 2'. A sensor will remain off for 300 to 1800 seconds (5 minutes to 1/2 hour). A sensor will remain on for 1800 to 3600 seconds (1/2 hour to 1 hour). Sensors will only be active when the 'Away Mode' switch is turned on.
 
-Parameters - (defaults in parenthesis)
+In this example, you use your automation software to turn the 'Away Mode' switch on when you want the sensors to be active (turn on & off), and off when you don't want them to be active.
+##### HomeKit
+In Home (or any of the HomeKit compatible apps), you will see three new devices exposed:
+* 1 switch named 'Away Mode'
+* 2 motion sensors named 'Trigger 1' and 'Trigger 2'
 
-* name - The name of the switch. ("Away Mode")
-* sensorNames - Array of names for each sensor to be created. (["Trigger 1"])
-* minOffTime - Minimum off time (seconds). (300)
-* maxOffTime - Maximum off time (seconds). (1800)
-* minOnTime - Minimum on time (seconds). (1800)
-* maxOnTime - Maximum off time (seconds). (3600)
+Automate the switch to turn on and off at specific times. For example:
+* 15 minutes before sunset (daily), turn the switch on
+* At 10:00pm (daily), turn the switch off
 
+Automate the motions sensors. For Example:
+* When motion is detected ('Trigger 1'), turn 'Lamp 1' on
+* When no motion is detected ('Trigger 1'), turn 'Lamp 1' off
+
+This setup makes sense when you know you will be away from your house for an extended period of time. You enable the switch automation before you leave and disable it when you return.
+
+### Advanced configuration example
+config.json:
+```
+"accessories": [
+    {
+        "accessory": "AwayMode",
+        "name": "Away Mode",
+        "sensorNames": [
+            "Trigger 1",
+            "Trigger 2"
+        ],
+        "minOffTime": 300,
+        "maxOffTime": 1800,
+        "minOnTime": 1800,
+        "maxOnTime": 3600,
+        "startTime": "sunset",
+        "endTime": "22:00",
+        "location": {
+            "lat": 40.689510,
+            "long": -74.044500
+        },
+        "offset": {
+            "sunrise": 0,
+            "sunset": -15
+        }
+    }
+]
+```
+The above example creates a switch called 'Away Mode' and two sensors: 'Trigger 1' and 'Trigger 2'. A sensor will remain off for 300 to 1800 seconds (5 minutes to 1/2 hour). A sensor will remain on for 1800 to 3600 seconds (1/2 hour to 1 hour). Sensors will only be active when the 'Away Mode' switch is turned on. Sensors will only turn on from 15 minutes before sunset to 10:00pm. The location information is used to compute the values for sunrise/sunset (as needed).
+
+In this example, you might use your automation software to detect when your residence is not occupied and turn on the 'Away Mode' and turn if off when the residence is occupied. However, you only want the sensors turning on and off during a specific period of the day.
+
+**Note**: This can *mostly* be accomplished using HomeKit and the simple configuration. However, HomeKit does not allow you to mix specific times (eg., 10:00pm) with sunrise/sunset times.
+
+##### HomeKit
+In Home (or any of the HomeKit compatible apps), you will see three new devices exposed:
+* 1 switch named 'Away Mode'
+* 2 motion sensors named 'Trigger 1' and 'Trigger 2'
+
+Automate the switch to turn on and off based on specific conditions. For example:
+* When the last person leaves home, turn the switch on
+* When the first person arrives home, turn the switch off
+
+Automate the motions sensors. For Example:
+* When motion is detected ('Trigger 1'), turn 'Lamp 1' on
+* When no motion is detected ('Trigger 1'), turn 'Lamp 1' off
+
+This setup makes sense when you want to make sure the automation always runs when you are not home, but only want the sensors to trigger during specific times of the day.
+
+### Parameters
+
+| Param         | Description   | Default  |
+| ------------- | ------------- | -------- |
+| accessory | **Must be set to "AwayMode"**
+| name | The name of the switch | "Away Mode" |
+| sensorNames | Array of names for each sensor to be created | ["Trigger 1"] |
+| minOffTime | Minimum off time (secs) | 300 |
+| maxOffTime | Maximum off time (secs) | 1800 |
+| minOnTime | Minimum on time (secs) | 1800 |
+| maxOnTime | Maximum on time (secs) | 3600 |
+| startTime | Time at which triggers should start to fire<br>("hh:mm"\|"sunrise"\|"sunset") | "00:00" |
+| endTime | Time at which triggers should stop firing<br>("hh:mm"\|"sunrise"\|"sunset")| "23:59" |
+| location | Lat/long location to compute sunrise/sunset from. Use in conjunction with "startTime"\|"endTime" when they are set to "sunrise"\|"sunset".<br>({"lat": x, "long": y}) <br><br>Find your location: [Google Maps location finder](https://google-developers.appspot.com/maps/documentation/utils/geocoder/) | {"lat": 0, "long": 0} |
+| offset | Offset information for sunrise/sunset. Offset is in minutes. May be negative (before) or positive (after). Use in conjunction with "startTime"\|"endTime" when they are set to "sunrise"\|"sunset".<br>({"sunrise": mins, "sunset": mins}) | {"sunrise": 0, "sunset": 0} |
+
+Parameters in the table above are optional, except 'accessory' which must be set to 'AwayMode', and need not be specified if you are happy with the default value.
+
+Time examples:
+| startTime | endTime | Offset |Description |
+| --------- | ------- | ------ | ----------- |
+| "08:00" | "20:00" | NA | Span the hours from 8am to 8pm |
+| "20:00" | "08:00" | NA | Span the hours from 8pm to 8am |
+| "sunset" | "22:00" | {"sunset":-15} | Span the hours from 15 minutes before sunset to 10pm. Sunset is computed based on the location you provide. |
+| "23:00" | "sunrise" | {"sunrise":30} | Span the hours from 11pm to 30 minutes after sunrise. Sunrise is computed based on the location you provide. |
+| "sunrise" | "sunset" | {"sunrise":-15, "sunset":15} | Span the hours from 15 minutes before sunrise to 15 minutes after sunset. Sunrise and sunset are computed based on the location you provide. |
--- index.js
+++ index.js
@@ -22,8 +22,18 @@
 // maxOffTime - Maximum off time (seconds). (1800)
 // minOnTime - Minimum on time (seconds). (1800)
 // maxOnTime - Maximum off time (seconds). (3600)
+// startTime - Time at which triggers should start to fire (hh:mm|sunrise|sunset). (00:00)
+// endTime - Time at which triggers should stop firing (hh:mm|sunrise|sunset). (23:59)
+// location - lat/long location to compute sunrise/sunset from ({lat: x, long: y}). ({lat: 0, long: 0})
+// offset - Offset information for sunrise/sunset ({sunrise: mins, sunset: mins}). ({sunrise: 0, sunset: 0})
+//
+// User location can be found with:
+//     https://google-developers.appspot.com/maps/documentation/utils/geocoder/
 //
 ////////////////////////////////////////////////////////////////////////////////
+
+var TimeFormat = require('hh-mm-ss');
+var SunCalc = require('suncalc');
 
 var Service, Characteristic;
 
@@ -45,10 +55,20 @@ class AwayMode {
         this.sensorNames = config["sensorNames"] || ["Trigger 1"];
 
         // Time in seconds
-        this.minOffTime = config["minOffTime"] || 300;  // 5 min
-        this.maxOffTime = config["maxOffTime"] || 1800; // 30 min
-        this.minOnTime = config["minOnTime"] || 1800;   // 30 min
-        this.maxOnTime = config["maxOnTime"] || 3600;   // 60 min
+        this.minOffTime = config["minOffTime"] || 300;  // 5 min (secs)
+        this.maxOffTime = config["maxOffTime"] || 1800; // 30 min (secs)
+        this.minOnTime = config["minOnTime"] || 1800;   // 30 min (secs)
+        this.maxOnTime = config["maxOnTime"] || 3600;   // 60 min (secs)
+
+        this.startTime = config["startTime"] || "00:00"; // hh:mm|sunrise|sunset
+        this.endTime = config["endTime"] || "23:59";     // hh:mm|sunrise|sunset
+
+        // https://google-developers.appspot.com/maps/documentation/utils/geocoder/
+        this.location = config["location"] || {lat:0, long:0};
+        this.offset = config["offset"] || {sunrise:0, sunset:0}; // 0 min (mins)
+
+        // This call computes side-effects.
+        this.computeStartEndTimes();
 
         // Multiplier to get to timer values (in milliseconds)
         this.multiplier = 1000;
@@ -88,6 +108,113 @@ class AwayMode {
     }
 
     //
+    // Convert seconds to "hh:mm:ss" for display
+    //
+    secondsToHourMinSec(timeInSeconds) {
+        let r = 0;
+
+        // hours
+        let hours = Math.floor(timeInSeconds / 3600);
+
+        // minutes
+        r = timeInSeconds % 3600
+        let mins = Math.floor(r / 60);
+
+        // seconds
+        r = r % 60;
+
+        return '' + hours.toString().padStart(2, '0') + ':' +
+               mins.toString().padStart(2, '0') + ':' +
+               r.toString().padStart(2, '0');
+    }
+
+    //
+    // Compute the seconds from midnight for the given
+    // time specification. Time is either 'hh:mm', 'sunrise', or 'sunset'.
+    // 'sc' contains sunrise/sunset info. 'offset' only applies to
+    // sunrise/sunset calculations.
+    //
+    computeSecondsFromMidnight(sc, time, offset) {
+      let seconds = 0;
+      let dynamic = false;
+
+      if (time === 'sunrise' || time === 'sunset') {
+          dynamic = true;
+          let t = 0; // base time - sunrise or sunset
+          let o = 0; // offset from t
+          if (time === 'sunrise') {
+              t = sc.sunrise;
+              o = offset.sunrise * 60; // to seconds
+          } else {
+              t = sc.sunset;
+              o = offset.sunset * 60; // to seconds
+          }
+          seconds = (t.getHours() * 3600) + (t.getMinutes() * 60) + t.getSeconds() + o;
+      } else {
+          seconds = TimeFormat.toS(time, 'hh:mm');
+      }
+
+      return { seconds: seconds, dynamic: dynamic };
+    }
+
+    //
+    // Compute start and end times. If dynamic values present (sunrise | sunset),
+    // this method will be called at midnight every day.
+    //
+    // *** Side effects ***
+    // Sets the values for this.startTime and this.endTime
+    //
+    computeStartEndTimes() {
+        let dynamic = false;
+
+        let times = SunCalc.getTimes(new Date(), this.location.lat, this.location.long);
+
+        let startInfo = this.computeSecondsFromMidnight(times, this.startTime, this.offset);
+        this.startSeconds = startInfo.seconds;
+        dynamic = startInfo.dynamic;
+
+        let endInfo = this.computeSecondsFromMidnight(times, this.endTime, this.offset);
+        this.endSeconds = endInfo.seconds;
+        dynamic = dynamic || endInfo.dynamic;
+
+        this.log("Start: " + this.secondsToHourMinSec(this.startSeconds) +
+                 " End: " + this.secondsToHourMinSec(this.endSeconds));
+
+        // Set a timer to expire at midnight so we can recalculate the
+        // values of sunrise & sunset (if needed)
+        if (dynamic) {
+            let today = new Date();
+            let tommorow = new Date(today.getFullYear(),today.getMonth(),today.getDate()+1);
+            let timeToMidnight = (tommorow-today)+5000; // +5sec to push it past
+            this.log("Recompute in: " + timeToMidnight/1000);
+            let timer = setTimeout(function() {
+                this.computeStartEndTimes();
+            }.bind(this), timeToMidnight);
+        }
+    }
+
+    //
+    // Return true if the sensor should be turned on. I.e., now falls
+    // withing the startTime / endTime range.
+    //
+    sensorOnTime() {
+        let now = new Date();
+        let currentSeconds = (now.getHours() * 3600) + (now.getMinutes() * 60) + now.getSeconds();
+
+        this.log("[" + this.secondsToHourMinSec(this.startSeconds) + " - " + this.secondsToHourMinSec(this.endSeconds) + "] --> " + this.secondsToHourMinSec(currentSeconds));
+
+        // start / end w/in same day, e.g. 08:00 - 20:00
+        if (this.startSeconds <= this.endSeconds) {
+            return (this.startSeconds < currentSeconds) && (currentSeconds < this.endSeconds);
+        }
+
+        // start / end span days, e.g. 20:00 - 08:00
+        else {
+            return (this.startSeconds < currentSeconds) || (currentSeconds < this.endSeconds);
+        }
+    }
+
+    //
     // Turn sensor on after a random amount of off time.
     //
     startOnTimer(id) {
@@ -95,12 +222,18 @@ class AwayMode {
         const serviceState = this.serviceStates[id];
 
         let time = parseInt(Math.floor(Math.random() * (this.maxOffTime - this.minOffTime + 1) + this.minOffTime));
-        this.log("Starting on timer for sensor: " + id + ", delay: " + time);
+        let now = new Date();
+        let currentSeconds = (now.getHours() * 3600) + (now.getMinutes() * 60) + now.getSeconds();
+        this.log("Starting on timer for sensor: " + id + ", delay: " + time +
+                 " [" + this.secondsToHourMinSec(currentSeconds + time) + "]");
 
         serviceState.timeout = setTimeout(function() {
-            this.log("Turning motion on for sensor: " + id);
-            sensor.setCharacteristic(Characteristic.MotionDetected, true);
-            serviceState.motionDetected = true;
+            // Only turn on sensors during allowed times
+            if (this.sensorOnTime()) {
+                this.log("Turning motion on for sensor: " + id);
+                sensor.setCharacteristic(Characteristic.MotionDetected, true);
+                serviceState.motionDetected = true;
+            }
 
             this.startOffTimer(id);
         }.bind(this), time*this.multiplier);
@@ -114,7 +247,10 @@ class AwayMode {
         const serviceState = this.serviceStates[id];
 
         let time = parseInt(Math.floor(Math.random() * (this.maxOnTime - this.minOnTime + 1) + this.minOnTime));
-        this.log("Starting off timer for sensor: " + id + ", delay: " + time);
+        let now = new Date();
+        let currentSeconds = (now.getHours() * 3600) + (now.getMinutes() * 60) + now.getSeconds();
+        this.log("Starting off timer for sensor: " + id + ", delay: " + time +
+                 " [" + this.secondsToHourMinSec(currentSeconds + time) + "]");
 
         serviceState.timeout = setTimeout(function() {
             this.log("Turning motion off for sensor: " + id);
